e20aba6b067a836bd89b0cee012de3be1cc3b745
cokid1998 <cokid98@naver.com>
2024-07-26 00:19:59 +0900

    Add category korean vertical divider

--- src/components/Category/Category.tsx
+++ src/components/Category/Category.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { CategoryType } from "@src/types/categoryType";
 import CategoryItem from "@src/components/Category/CategoryItem";
+import { Separator } from "@src/components/ui/separator";
 
 interface CategoryProps {
   categories: CategoryType[];
@@ -8,9 +9,10 @@ interface CategoryProps {
 
 function Category({ categories }: CategoryProps) {
   return (
-    <div className="flex gap-[16px] px-[16px]">
+    <div className="flex gap-[16px] px-[16px] items-center">
+      <div className="text-[18px] text-[#22C564] font-bold">카테고리</div>
+      <Separator orientation={"vertical"} className="h-[20px] w-[2px]" />
       {categories.map(({ category, id }) => {
-        console.log(categories);
         return <CategoryItem key={id} categoryName={category} />;
       })}
     </div>
